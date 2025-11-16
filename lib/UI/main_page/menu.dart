061147cf896fd7ca2dada16db72aede6e07bdb69
Alex09ander <alexandercop0701@gmail.com>
2025-11-16 19:47:23 +0100

    First menu update
    Pierwszy zarys wyglądu menu

--- lib/UI/main_page/menu.dart
+++ lib/UI/main_page/menu.dart
@@ -3,7 +3,9 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:your_advisor/UI/custom_widgets/custom_square_btn.dart';
 import 'package:your_advisor/domain/app_colors.dart';
+
 
 class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
@@ -16,9 +18,92 @@ class MenuPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
+            SizedBox(height: 50,),
             Align(
               alignment: Alignment.center,
               child: SvgPicture.asset("assets/svg/Logo_test-removebg-preview.svg", width: 70,),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 50, 10, 0),
+                  child: CustomSquareBtn(
+                    text: "Tryb\npsychologa",
+                    iconHeight: 100,
+                    fontSize: 30,
+                    sBoxSize: 50,
+                    sBoxSize2: 20,
+                    mIconPath: "assets/svg/Logo_test-removebg-preview.svg",
+                    bgColor: AppColors.secondaryColor,
+                    mHeight: 300,
+                    mWidth: 180,
+                    onTap: () {
+
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 50, 0, 0),
+                  child: CustomSquareBtn(
+                    text: "Tryb\ndoradcy",
+                    iconHeight: 100,
+                    fontSize: 30,
+                    sBoxSize: 50,
+                    sBoxSize2: 20,
+                    mIconPath: "assets/svg/Logo_test-removebg-preview.svg",
+                    bgColor: AppColors.secondaryColor,
+                    mHeight: 300,
+                    mWidth: 180,
+                    onTap: () {
+
+                    },
+                  ),
+                )
+
+
+              ]
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 50, 10, 0),
+                    child: CustomSquareBtn(
+                      text: "Inny\ntryb",
+                      iconHeight: 100,
+                      fontSize: 30,
+                      sBoxSize: 50,
+                      sBoxSize2: 20,
+                      mIconPath: "assets/svg/Logo_test-removebg-preview.svg",
+                      bgColor: AppColors.secondaryColor,
+                      mHeight: 300,
+                      mWidth: 180,
+                      onTap: () {
+
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 50, 0, 0),
+                    child: CustomSquareBtn(
+                      text: "Inny\ntryb",
+                      iconHeight: 100,
+                      fontSize: 30,
+                      sBoxSize: 50,
+                      sBoxSize2: 20,
+                      mIconPath: "assets/svg/Logo_test-removebg-preview.svg",
+                      bgColor: AppColors.secondaryColor,
+                      mHeight: 300,
+                      mWidth: 180,
+                      onTap: () {
+
+                      },
+                    ),
+                  )
+
+
+                ]
             )
           ],
         ),
